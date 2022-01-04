b2563341a7c859632eb0f75185318940410ae4ca
Joshua Perrin <joshuaperrin@outlook.com>
2022-01-04 14:48:33 +1100

    Update dayTenPuzzle.R

--- dayTenPuzzle.R
+++ dayTenPuzzle.R
@@ -5,28 +5,112 @@ rm(list = ls())
 
 syntax <- readLines("dayTenInput.txt")
 
-substr(syntax[1], 1, 1)
+delimiterStack = c()
 
-triangleBracket <- 0
-squareBracket <- 0
-curvedBracket <- 0
-curlyBrack <- 0
+errorList = c("1")
+
+
+
+lastCheck <- function(o, c){
+  if(substr(syntax[i], j, j) == c){
+    if(tail(delimiterStack, 1) == o){
+      delimiterStack = delimiterStack[-length(delimiterStack)]
+    } else {
+      warning("help")
+      errorList = append(errorList, paste0(i))
+    }
+      
+    }
+  }
 
 
 for(i in 1:length(syntax)){
-  for(j in 1:nchar(syntax[i]){
-    if(substr(syntax[i], j, j) == "<")
-      triangleBracket = triangleBracket + 1
-  } else 
-    if(substr(syntax[i], j, j) == ">"{
-      triangleBracket = triangleBracket + 1
-    } else
-      if(substr(syntax[i], j, j) == "[")
-        squareBracket = squareBracket + 1
-} else 
-  if(substr(syntax[i], j, j) == "]"{
-    squareBracket = squareBracket + 1
-  } else
-      
-    
-  
+  delimiterStack = c()
+  for(j in 1:nchar(syntax[i])){
+    if(substr(syntax[i], j, j) %in% c("<", "(", "{", "[")){
+      delimiterStack = append(delimiterStack, substr(syntax[i], j, j))
+    }
+    lastCheck("[", "]")
+    lastCheck("(", ")")
+    lastCheck("{", "}")
+    lastCheck("<", ">")
+  }
+}
+
+# for(i in 1:length(syntax)){
+#   triangleBracket <- 0
+#   squareBracket <- 0
+#   curvedBracket <- 0
+#   curlyBracket <- 0
+#   for(j in 1:nchar(syntax[i])){
+#     if(substr(syntax[i], j, j) == "<"){
+#       triangleBracket = triangleBracket + 1
+#   } else 
+#     if(substr(syntax[i], j, j) == ">"){
+#       triangleBracket = triangleBracket - 1
+#     } else
+#       if(substr(syntax[i], j, j) == "["){
+#         squareBracket = squareBracket + 1
+# } else 
+#   if(substr(syntax[i], j, j) == "]"){
+#     squareBracket = squareBracket - 1
+#   } else
+#     if(substr(syntax[i], j, j) == "("){
+#       curvedBracket = curvedBracket + 1
+#     } else 
+#       if(substr(syntax[i], j, j) == ")"){
+#         curvedBracket = curvedBracket - 1
+#       } else
+#         if(substr(syntax[i], j, j) == "{"){
+#           curlyBracket = curlyBracket + 1
+#         } else 
+#           if(substr(syntax[i], j, j) == "}"){
+#             curlyBracket = curlyBracket - 1
+#           }
+#     if(curvedBracket == -1){
+#       break(print("Curly"))
+#     }
+#   }
+# }
+            
+# breaker = syntax[1] %>%
+#   str_extract_all("[\\[\\]]") %>%
+#   unlist()
+# 
+# counter = 0
+# 
+# for(j in 1:length(breaker)){
+#   counter = if_else(breaker[j] == "[", counter + 1, counter - 1)
+#   if(counter < 0){
+#     stop("Counter below zero.")
+#   }
+# }
+# 
+# errorList = c("1")
+# 
+# regex <- function(open, close, syntaxCount){
+#   breaker = syntax[syntaxCount] %>%
+#     str_extract_all(paste0("[\\", open, "\\", close, "]")) %>%
+#     unlist()
+#   
+#   counter = 0
+#   
+#   for(j in 1:length(breaker)){
+#     counter = if_else(breaker[j] == open, counter + 1, counter - 1)
+#     if(counter < 0){
+#       warning(paste0("Counter below zero for ", open, close, " in line ", syntaxCount))
+#     }
+#   }
+#   
+# }
+# 
+# for(i in 1:94){
+# regex("(", ")", i)
+# regex("[", "]", i)
+# regex("<", ">", i)
+# regex("{", "}", i)
+# }
+# 
+
+
+
